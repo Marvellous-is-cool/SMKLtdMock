71211f86050e2e311da678017ea0ccd818a93e82
Marvellous-is-cool <adebayoinioluwamarvellous1@gmail.com>
2023-03-11 20:47:17 +0100

    v8 - [bsV5 used]

--- js/index.js
+++ js/index.js
@@ -1,5 +1,29 @@
-// const resize = document.querySelector(".box_to_resize");
+let indShipInd = document.querySelector(".ind-shipInd");
+let indShipSec = document.querySelector(".individual_ships");
+let ISTemp = document.querySelector(".example");
 
 // let Hresolution,
 //   Wresolution = height,
 //   width;
+
+const cloneIndShips = (access_name) => {
+  const cloneTemp = ISTemp.cloneNode(true);
+  indShipSec.appendChild(cloneTemp);
+  cloneTemp.children[1].innerHTML = access_name;
+  console.log(cloneTemp.children[0].children[1]);
+};
+
+const SmallScreenSizes = () => {
+  indShipInd.classList.remove("d-none");
+  indShipSec.classList.add("d-none");
+};
+
+if (screen.width <= 480) {
+  SmallScreenSizes;
+}
+
+const names = ["Niaz Hossain", "Mortaza", "F.M. Zukov"];
+
+for (let i = 0; i <= names.length - 1; i++) {
+  cloneIndShips(names[i]);
+}
